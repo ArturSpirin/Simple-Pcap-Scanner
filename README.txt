34083dedca025e3576a46a122d88d0d9491cdce4
Artur Spirin <a.spirin@hotmail.com>
2016-02-16 19:56:56 -0800

    Initial commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+This is a simple script that allows to search if a .pcap file contains certain values
+
+How to use:
+
+    0. You have to have tshark installed and TSHARK_HOME must be set
+    1. Call the script via python and tell the script where the .pcap file is located: python BigBrother.pcap "path/to/my.pcap"
+    2. You have an option to specify which key values you want to search for: python BigBrother.pcap "path/to/my.pcap" .us anchorfree hotspotshield vpn
+        2.1 If you do not specify the key values, it will use default ones: keyWords = [".us","anchorfree","hotspotshield","vpn","proxy","rss2search", "hsselite"]
+    3. Script will produce stdout if it finds any of the key values
+    4. Once scan is started, a text file from .pcap file will become available in the project directory
---
+++ Scanner.py
@@ -0,0 +1,46 @@
+__author__ = 'Artur Spirin'
+
+import os, sys
+
+class Scan:
+
+
+    def __init__(self, pcapFile=None):
+
+        self.file = pcapFile
+        self.keyWords = []
+
+        argNum = len(sys.argv)
+        print "Got", argNum, "arguments"
+
+        if(argNum > 1):
+            if(".pcap" in sys.argv[1]):
+                self.file = sys.argv[1]
+                count = 1
+                if(argNum > 2):
+                    while count != argNum-1:
+                        count+=1
+                        self.keyWords.append(sys.argv[count])
+                else:
+                    self.keyWords = [".us","anchorfree","hotspotshield","vpn","proxy","rss2search", "hsselite"]
+            else: print "using default"
+
+        print "Working with file:", self.file
+        print "Using key words: ", self.keyWords
+
+    def pcap(self):
+
+        tsharkHome = os.getenv("TSHARK_HOME")
+        if(tsharkHome==None): print "TSHARK_HOME not found! Make sure to set TSHARK_HOME env variable."
+        else: print "TSHARK_HOME found at:", tsharkHome
+
+        os.system('{} -V -r {} > pcapText.txt'.format(tsharkHome, self.file))
+
+        with open('pcapText.txt', 'r+') as File:
+            data = File.read().splitlines()
+            for line in data:
+                for key in self.keyWords:
+                    if key in line.lower():
+                        print "Found: ", key, " in line: ",line
+
+Scan().pcap()
